205a0f15f0bc84d8788560377a8181dc360c507c
SaheeqAhmad <SaheeqAhmad1@gmail.com>
2020-06-13 19:00:23 +0500

    Initial Commit

---
+++ src/Room.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function Room() {
+    let [isLit, setLit] = useState(true)
+    let [age, setAge] = useState(25)
+    // function updatelit() {
+    //     console.log('Button Click');
+    //     setLit(!isLit)
+    //     // setAge(++age)
+    // }
+    // const updateAge = () => {
+    //     console.log('Button Click');
+    //     //setLit(!isLit)
+    //     setAge(++age)
+    // }
+    //const state = useState(true)
+    //console.log("State = ", state);
+    return (
+        <div>
+            This paragraph is {isLit ? "lit" : "dark"}
+            <br />
+            Age is {age}
+            <br />
+            <button onClick={() => setLit(!isLit)}> Toggle Light</button>
+            <br />
+            <button onClick={() => {
+                console.log('Button Click');
+                setAge(++age)
+            }}> Increase Age</button>
+        </div>
+    );
+}
+
+export default Room;
